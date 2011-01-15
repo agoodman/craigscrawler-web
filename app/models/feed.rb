374d27559d238927a632e07b6355b32fb267f539
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-01-14 22:57:22 -0500

    added last_item_published_at method

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,12 +1,16 @@
 class Feed < ActiveRecord::Base
 
   belongs_to :user
-  has_many :items, :dependent => :destroy
+  has_many :items, :dependent => :destroy, :order => 'published_at desc'
   has_many :keywords, :dependent => :destroy
   
   validates_presence_of :user_id
 
   REGIONS = { 'tampa' => 'Tampa' }
   CATEGORIES = { 'mca' => 'Motorcycles' }
+
+  def last_item_published_at
+    items.first.published_at
+  end
   
 end
