223239b51e36dbfc18c2c43e10389ae475c7562e
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-01-15 14:59:44 -0500

    added users controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,44 @@
 class UsersController < Clearance::UsersController
   
+  before_filter :authenticate, :only => [ :edit, :show, :update, :destroy ]
+
+  def edit
+    @user = current_user
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def show
+    @user = current_user
+    respond_to do |format|
+      format.json { render :json => @user }
+    end
+  end
+  
+  def update
+    @user = current_user
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to account_path, :notice => 'Account Updated' }
+        format.json { head :ok }
+      else
+        format.html { redirect_to account_path, :alert => @user.errors.full_messages.join('<br/>') }
+        format.json { render :json => { :errors => @user.errors.full_messages }, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @user = current_user
+    sign_out
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { head :ok }
+    end
+  end
+  
   private
   
   def url_after_create
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,8 +1,91 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  context "when not signed in" do
+    setup do
+      @user = Factory(:user)
+      sign_out
+    end
+    
+    context "on post create" do
+      setup { post :create, :user => Factory.attributes_for(:user) }
+      should ('redirect to feeds'){redirect_to feeds_path}
+    end
+    
+    context "on get edit" do
+      setup { get :edit }
+      should ('redirect to sign in'){redirect_to sign_in_path}
+    end
+    
+    context "on put update" do
+      setup { put :update, :user => Factory.attributes_for(:user) }
+      should ('redirect to sign in'){redirect_to sign_in_path}
+    end
+    
+    context "on delete destroy" do
+      setup { delete :destroy }
+      should ('redirect to sign in'){redirect_to sign_in_path}
+    end
+    
+    context "on post create as json" do
+      setup { post :create, :format => 'json', :user => Factory.attributes_for(:user) }
+      should respond_with :success
+    end
+    
+    context "on get show as json" do
+      setup { get :show, :format => 'json' }
+      should respond_with :unauthorized
+    end
+    
+    context "on put update as json" do
+      setup { put :update, :format => 'json', :user => Factory.attributes_for(:user) }
+      should respond_with :unauthorized
+    end
+    
+    context "on delete destroy as json" do
+      setup { delete :destroy, :format => 'json' }
+      should respond_with :unauthorized
+    end
   end
+  
+  context "when signed in" do
+    setup do
+      @user = Factory(:user)
+      sign_in_as @user
+    end
+    
+    context "on get edit" do
+      setup { get :edit }
+      should respond_with :success
+      should assign_to :user
+    end
+    
+    context "on put update" do
+      setup { put :update, :user => Factory.attributes_for(:user) }
+      should ('redirect to user'){redirect_to account_path}
+    end
+    
+    context "on delete destroy" do
+      setup { delete :destroy }
+      should ('redirect to root'){redirect_to root_path}
+    end
+    
+    context "on get show as json" do
+      setup { get :show, :format => 'json' }
+      should respond_with :success
+      should assign_to :user
+    end
+    
+    context "on put update as json" do
+      setup { put :update, :format => 'json', :user => Factory.attributes_for(:user) }
+      should respond_with :success
+    end
+    
+    context "on delete destroy as json" do
+      setup { delete :destroy, :format => 'json' }
+      should respond_with :success
+    end
+  end
+  
 end
