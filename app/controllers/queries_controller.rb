73d4b35bfa3299fdc55b64d8a2b3048e63b078b0
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-05-24 18:09:40 -0400

    explicitly set order of recent queries

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,7 +1,7 @@
 class QueriesController < ApplicationController
 
   def index
-    @recent_queries = Query.where(['created_at > ?',Time.now-1.day]).limit(10)
+    @recent_queries = Query.where(['created_at > ?',Time.now-1.day]).limit(10).order("created_at desc")
     @top_keywords = Keyword.includes(:queries).all.sort {|x,y| y.queries.count <=> x.queries.count}.first(10)
   end
 
