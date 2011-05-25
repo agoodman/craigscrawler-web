699be6feb9a79ae4b71cfdbc6c217810ba4e86ea
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-05-25 18:29:46 -0400

    fixed naming error (categories -> children)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,8 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.includes(:categories).where(:parent_id => nil)
-    category_hashes = @categories.collect {|c| {:code => c.code, :title => c.title, :children => c.categories.collect {|s| {:code => s.code, :title => s.title}}}}
+    @categories = Category.includes(:children).where(:parent_id => nil)
+    category_hashes = @categories.collect {|c| {:code => c.code, :title => c.title, :children => c.children.collect {|s| {:code => s.code, :title => s.title}}}}
     respond_to do |format|
       format.json { render :json => category_hashes.to_json }
     end
