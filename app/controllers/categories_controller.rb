38314dbc00501c0589f4f84ee6649fef185d579e
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-05-27 11:37:54 -0400

    ordered categories alphabetically by title

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.includes(:children).where(:parent_id => nil)
+    @categories = Category.includes(:children).order('title asc').where(:parent_id => nil)
     category_hashes = @categories.collect {|c| {:code => c.code, :title => c.title, :children => c.children.collect {|s| {:code => s.code, :title => s.title}}}}
     respond_to do |format|
       format.json { render :json => category_hashes.to_json }
