610b46ac90821387e5c8a27cf85713da782f5ed9
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-09-29 14:28:32 -0400

    added item[link] param to satisfy emails#index functional test

--- test/functional/emails_controller_test.rb
+++ test/functional/emails_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class EmailsControllerTest < ActionController::TestCase
 
   context "on get index as json" do
-    setup { get :index, :format => 'json' }
+    setup { get :index, :format => 'json', :item => { :link => "http://tampa.craigslist.org/hil/bab/2623847785.html" } }
     should respond_with :success
   end
   
